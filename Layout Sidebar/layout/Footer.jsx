6ca2cfac69602425a669655d89ef265f731fb2f5
abdullukmana17-id <abdullukmana17@gmail.com>
2026-05-20 10:56:54 +0700

    Update Footer.jsx

--- Layout Sidebar/layout/Footer.jsx
+++ Layout Sidebar/layout/Footer.jsx
@@ -1,5 +1,15 @@
 export default function Footer() {
     return(
-        <></>
+        <>
+            <footer  className="vh-100 border-top">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+
+                        </div>
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
