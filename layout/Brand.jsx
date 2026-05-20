66643a88494551c79f0ef28c805cd3d8173b0f2b
abdullukmana17-id <abdullukmana17@gmail.com>
2026-05-20 08:31:25 +0700

    Update Brand.jsx

--- layout/Brand.jsx
+++ layout/Brand.jsx
@@ -2,8 +2,8 @@ import { useContext } from "react"
 
 export default function Brand() {
     return(
-        <a href="#" className="navbar-brand link-dark text-decoration-none fs-5 font-serif">
-            Tahfidzul Quran
+        <a href="#" className="navbar-brand text-decoration-none fs-5">
+            My App
         </a>
     )
 }
