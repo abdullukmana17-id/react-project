e4c784d9e0295f48d95abb3b0426bf3a1d1a563a
abdullukmana17-id <abdullukmana17@gmail.com>
2026-05-20 09:18:11 +0700

    Delete layout directory

--- layout/Brand.jsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react"
-
-export default function Brand() {
-    return(
-        <a href="#" className="navbar-brand text-decoration-none fs-5">
-            My App
-        </a>
-    )
-}
