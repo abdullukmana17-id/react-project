b792a3d6fb9f8f0c800da64c2c6509df3233df4f
abdullukmana17-id <abdullukmana17@gmail.com>
2026-05-20 10:56:42 +0700

    Update Brand.jsx

--- Layout Sidebar/layout/Brand.jsx
+++ Layout Sidebar/layout/Brand.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react"
 
 export default function Brand() {
     return(
-        <a href="#" className="navbar-brand text-decoration-none fs-5">
+        <a href="#" className="navbar-brand text-decoration-none">
             My App
         </a>
     )
